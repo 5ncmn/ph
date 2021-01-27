088d450e9b45a6f52de1d70f3e5cc712e7d75525
senecamanu <sncmn@sncmn.local>
2021-01-28 04:04:59 +0700

    initial commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo su
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+sudo apt-get update -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install vim tmux lshw -y
+vim start_miner.sh
